3300f5c3f25b3c62e3bf5e1396be325adbf9f33c
Ubuntu <linux_user@MXAPAVMPRDL.c2ftiuk1gwlendkh2nojt1z02g.bx.internal.cloudapp.net>
2021-11-19 21:11:10 +0000

    First upload

---
+++ sqlconnection.py
@@ -0,0 +1,18 @@
+import pyodbc
+server = 'mxbdsqlprd.database.windows.net'
+database = 'MXBDSQLPRD'
+username = 'user4_sql'
+password = 'GEPP2021*'   
+driver= '{ODBC Driver 17 for SQL Server}'
+
+with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
+        row = cursor.fetchone()
+        while row:
+            print (str(row[0]) + " " + str(row[1]))
+            row = cursor.fetchone()
+
+'''
+python3 sqlconnection.py 
+'''
